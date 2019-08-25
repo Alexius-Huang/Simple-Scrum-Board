22d7fa7aabceba93b330844e4108683bae2b66a3
Maxwell Alexius <destinydragon85614@gmail.com>
2019-08-26 00:26:58 +0800

    Using ReactiveForm and able to create new Task

--- src/app/board.service.ts
+++ src/app/board.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Board } from './boards/board.model';
+import { Task } from './boards/task.model';
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +22,10 @@ export class BoardService {
 
   fetchTasks(): Observable<any> {
     return this.http.get('/api/tasks');
+  }
+
+  createTask(task: Task) {
+    return this.http.post<Task>('/api/tasks', task, this.httpOptions);
   }
 
   updateBoard(id: number, board: Board) {
--- src/app/boards/boards.component.ts
+++ src/app/boards/boards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Board } from './board.model';
 import { BoardService } from '../board.service';
 import { Task } from './task.model';
@@ -15,6 +16,16 @@ export class BoardsComponent implements OnInit {
   boardEditingId?: number;
   cacheBoardName?: string;
 
+  newTaskBoardId?: number;
+  newTaskForm;
+  newTaskFormErrors?: {
+    title: object,
+    description: object
+  } = {
+    title: {},
+    description: {},
+  };
+
   get boardTaskMap(): Map<number, Task> {
     const { boards, tasks } = this;
     const mapping = boards.reduce((acc, board) =>
@@ -29,7 +40,10 @@ export class BoardsComponent implements OnInit {
     return mapping;
   }
 
-  constructor(private boardService: BoardService) {}
+  constructor(
+    private boardService: BoardService,
+    private formBuilder: FormBuilder,
+  ) {}
 
   ngOnInit() {
     this.fetchBoards();
@@ -46,8 +60,37 @@ export class BoardsComponent implements OnInit {
       .subscribe(data => this.tasks = data);
   }
 
-  createTask(): void {
-    console.log('Create item');
+  newTask(boardId: number): void {
+    this.newTaskBoardId = boardId;
+    this.newTaskForm = this.formBuilder.group({
+      boardId,
+      title: new FormControl(null, Validators.required),
+      description: new FormControl(null, Validators.required),
+    });
+  }
+
+  onNewTaskSubmit(data) {
+    const title = this.newTaskForm.get('title');
+    const description = this.newTaskForm.get('description');
+
+    this.newTaskFormErrors = {
+      title: title.errors || {},
+      description: description.errors || {},
+    };
+
+    if (this.newTaskForm.status !== 'INVALID') {
+      this.boardService.createTask(data)
+        .subscribe(task => {
+          this.tasks = [...this.tasks, task];
+          this.newTaskForm.reset();
+          this.newTaskBoardId = null;
+        });
+    }
+  }
+
+  clearNewTaskForm() {
+    this.newTaskForm.reset();
+    this.newTaskBoardId = null;
   }
 
   editBoard(boardId: number): void {
