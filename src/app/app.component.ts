17b12d291fa7fd2780e7de1faa4dd3c091e66f1f
Maxwell Alexius <destinydragon85614@gmail.com>
2019-08-25 20:22:56 +0800

    Set up the json-server files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'simple-scrum-board';
+  title: string = 'Simple Scrum Board';
 }
